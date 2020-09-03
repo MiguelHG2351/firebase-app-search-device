49bc4f782a36cf3c388d52381d585d305737f8dc
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-09-02 21:41:07 -0600

    error source map

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require("./webpack.config.js")
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: "inline-source-map",
+    devtool: "source-map",
     devServer: {
         contentBase: './build'
     }
